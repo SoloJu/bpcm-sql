1ebd21fa0f062aa03d39e1a81672b96725965cf5
SoloJu <soloju@gmail.com>
2024-06-06 11:00:12 +0800

    changes3

--- labor.sql
+++ labor.sql
@@ -3,12 +3,12 @@
 -- ----------------------------
 CREATE TABLE `labor_wages`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
-  `logged_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登记时间',
+  `logged_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登记日期',
   `team` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '班组',
   `amount` decimal(12, 2) NOT NULL COMMENT '金额',
   `tax` decimal(12, 2) NOT NULL DEFAULT 0.000000 COMMENT '税率',
   `tax_amount` decimal(12, 2) NOT NULL COMMENT '含税金额',
-  `type` tinyint NOT NULL DEFAULT 1 COMMENT '工资类型,实名制-1, 非实名制-2, 点工转账-3, 临设-4',
+  `type` tinyint NOT NULL DEFAULT 1 COMMENT '付款方式,实名制-1, 非实名制-2, 点工转账-3, 临设-4',
   `project_id` bigint NOT NULL COMMENT '项目部ID',
   `supplier_id` bigint NOT NULL COMMENT '供应商ID',
   `notes` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '备注',
@@ -19,20 +19,20 @@ CREATE TABLE `labor_wages`  (
   `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
   `tenant_id` bigint NOT NULL DEFAULT 1 COMMENT '租户编号',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 101 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '材料收货单';
+) ENGINE = InnoDB AUTO_INCREMENT = 101 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '劳务工资单';
 
 -- ----------------------------
 -- Table structure for labor_wages_import
 -- ----------------------------
 CREATE TABLE `labor_wages_import`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
-  `logged_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登记时间',
+  `logged_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登记日期',
   `team` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '班组',
   `amount` decimal(12, 2) NOT NULL COMMENT '金额',
   `tax` decimal(12, 2) NOT NULL DEFAULT 0.000000 COMMENT '税率',
   `tax_amount` decimal(12, 2) NOT NULL COMMENT '含税金额',
-  `type` tinyint NOT NULL DEFAULT 1 COMMENT '工资类型,实名制-1, 非实名制-2, 点工转账-3, 临设-4',
-  `type_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '类型名称',
+  `type` tinyint NOT NULL DEFAULT 1 COMMENT '付款方式,实名制-1, 非实名制-2, 点工转账-3, 临设-4',
+  `type_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '付款方式名称',
   `project_id` bigint NOT NULL COMMENT '项目部ID',
   `project_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '项目部名称',
   `supplier_id` bigint NOT NULL COMMENT '供应商ID',
@@ -48,4 +48,4 @@ CREATE TABLE `labor_wages_import`  (
   `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
   `tenant_id` bigint NOT NULL DEFAULT 1 COMMENT '租户编号',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 101 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '材料收货导入单';
+) ENGINE = InnoDB AUTO_INCREMENT = 101 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '劳务工资导入单';
